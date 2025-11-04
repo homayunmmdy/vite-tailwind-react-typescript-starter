b17ecda602203dd29b0cd9f0cc3c6d1bed41b9e7
Homayoun <homayoun763@gmail.com>
2025-11-04 22:43:58 +0330

    adding : tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 const App = () => {
-  return <h1>⚡ Vite React starter with TypeScript & Tailwind CSS</h1>;
+  return (
+    <h1 className="text-2xl text-center m-5 font-bold">
+      Vite React starter with TypeScript & Tailwind CSS
+    </h1>
+  );
 };
 
 export default App;
